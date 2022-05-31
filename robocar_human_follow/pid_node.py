57256a164bb976445fda113bbbd3d06e4043dde6
Martin Heir <mbheir@gmail.com>
2022-05-31 20:42:52 +0000

    sam and ray changes to pid_node

--- robocar_human_follow/pid_node.py
+++ robocar_human_follow/pid_node.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 29 19:25:21 2022
+
+@author: Team 2 robocar1
+"""
 from custom_interfaces.msg import Detection
 from geometry_msgs.msg import Twist
 import rclpy
@@ -12,7 +18,7 @@ from rclpy.qos import ReliabilityPolicy, QoSProfile
 # detection.box_height
 DETECTION_TOPIC_NAME = '/detections'
 
-# Check actuator package at https://gitlab.com/ucsd_robocar2/ucsd_robocar_actuator2_pkg/-/blob/master/ucsd_robocar_actuator2_pkg/vesc_twist_node.py
+# Check actuator package at https://urldefense.proofpoint.com/v2/url?u=https-3A__gitlab.com_ucsd-5Frobocar2_ucsd-5Frobocar-5Factuator2-5Fpkg_-2D_blob_master_ucsd-5Frobocar-5Factuator2-5Fpkg_vesc-5Ftwist-5Fnode.py&d=DwIGAg&c=-35OiAkTchMrZOngvJPOeA&r=RSuX6zNtjR8QyV9bB4B7mpCedielV-h7SUddZpjVIs8&m=dsI64WVV0y1GedjWrwkE-ZyyTn6UHEM3Y1l7i-W6jzU8qI-8zI3LNm1NE7Gn-nf2&s=ZK5G2HJG0eQ6j8moB7jqjsxUXnevRcu09GX80BHj_hg&e= 
 # TO RUN THE ACTUATOR ROS PACKAGE
 # ros2 launch ucsd_robocar_actuator2_pkg vesc_twist.launch.py
 # THIS WILL START THE ACTUATOR NODE LISTENING TO /cmd_vel
@@ -39,15 +45,22 @@ class PID_Node(Node):
 
     def detection_callback(self, msg):
         # REACT TO A DETECTION HERE
-
-        pass
-
+        self.x = msg.center_x
+        self.y = msg.center_y
+        # This publishes the value to the terminal
+        self.get_logger().info(str(self.x))
+        # This code attaches the values from self.x into steering commands
+        #if self.x < 0:
+        #self.cmd.linear.x = 0.05
+            #self.cmd.angular.x = 3
+     
+    
     def timer_callback(self):
         # WE WANT TO STOP THE CAR IF NO DETECTIONS FOR A CERTAIN AMOUNT OF TIME
 
         # Publish driving command example
         cmd = Twist()
-        cmd.linear.x = 0.05
+        cmd.linear.x = 0.1
 
         # Publishing the cmd_vel values to topipc
         self.publisher_.publish(cmd)
@@ -68,4 +81,6 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
+
+
 
